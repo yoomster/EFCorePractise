a4dfc0fde506078c8fac1ef269e6954582de41a5
yoomster <nperenboom@hotmail.com>
2024-05-03 22:30:43 +0200

    added models

---
+++ EFCorePractise/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace EFCorePractise.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    public string? Genre { get; set; }
+}
